e3cf8d7e465ee37b212e446c40ca279d49e6ff83
FrancoBagnasco <145878527+FrancoBagnasco@users.noreply.github.com>
2023-09-25 11:55:52 -0300

    Add files via upload

---
+++ notebooks/promedio_txt1(con error).py
@@ -0,0 +1,18 @@
+
+import re
+
+file = open("data/mbox-short.txt")
+data = []
+
+for line in file:
+    line = line.strip()
+    number = re.findall(r'\b\d+\b', line)
+    data.extend(number)
+    
+data = [int(number) for number in data]
+    
+promedio = int(sum(data) / len(data))
+
+#print(data)
+
+print(f"El promedio de todos los numeros del archivo txt es: {promedio}")
